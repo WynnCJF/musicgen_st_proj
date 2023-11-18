0ab7c997c1d25ccfa78906626e7eb1ca965bf4bf
JIAFENG_CHEN <wynnchen@umich.edu>
2023-11-17 23:41:00 -0500

    Add sliders and more options

--- st_musicgen_app.py
+++ st_musicgen_app.py
@@ -3,16 +3,15 @@ import replicate
 import os
 import concurrent.futures
 
-OUTPUT_NUM = 3
 
-def call_replicate_api(text_prompt):
+def call_replicate_api(text_prompt, length):
     """Call MusicGen's Replicate API with text_prompt."""
     output = replicate.run(
         "meta/musicgen:7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906",
         input={
             "model_version": "melody",
             "prompt": text_prompt,
-            "duration": 5,
+            "duration": length,
         }
     )
     
@@ -41,8 +40,8 @@ with st.sidebar:
     st.markdown("## Select an input mode:")
     
     input_mode_options = (
-        "Customize text input",
         "Choose from a pre-defined prompt",
+        "Customize text input",
     )
     
     input_mode_choice = st.radio(
@@ -52,6 +51,26 @@ with st.sidebar:
         key="input_mode_choice"
     )
     
+    st.divider()
+    
+    num_output = st.slider(
+        label="Number of generated outputs",
+        min_value=1,
+        max_value=5,
+        value=3,
+        step=1,
+        key="num_output",
+    )
+    
+    generate_length = st.slider(
+        label="Duration of generated audio (seconds)",
+        min_value=5,
+        max_value=30,
+        value=5,
+        step=1,
+        key="generate_length",
+    )
+    
 # Prompt input
 st.divider()
 
@@ -59,17 +78,20 @@ if st.session_state.input_mode_choice == "Customize text input":
     st.markdown("### Input your text prompt to MusicGen:")
     input_pmpt1 = st.text_input(
         label="Prompt",
-        value="Electric guitar solo, shoegaze, clean and clear recording quality, perfect for music production",
+        value="",
         key="pmpt1",
         label_visibility="collapsed",
+        placeholder="Electric guitar solo, shoegaze, clean and clear recording quality, perfect for music production"
     )
 else:
     st.markdown("### Choose a prompt:")
     pmpt_options = [
         "Electric guitar solo, shoegaze, clean and clear recording quality, perfect for music production",
         "Drum percussion solo, four-on-the-floor beat, tribal sound, traditional instrument, BPM: 125, clean and clear recording quality",
-        "Solo drum percussion with hi-hat, snare and kick drum",
-        "Synthesizer arpeggio with 80's retro feeling",
+        "Bell percussion solo, natural, organic, rhythmic, periodic, repetition, BPM: 125",
+        "Synth arpeggio, 80's retro, BPM: 95, solo performance",
+        "Piano solo, funky groove, chord progression, clean and clear recording quality, BPM: 128",
+        "Ambient pad, dark atmosphere, heavy reverb",
     ]
 
     input_pmpt_choice = st.selectbox(
@@ -92,8 +114,8 @@ if pmpt1_button:
     futures = []
     with st.spinner("Waiting for audio to be generated..."):
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            for _ in range(OUTPUT_NUM):
-                future = executor.submit(call_replicate_api, prompt)
+            for _ in range(int(st.session_state.num_output)):
+                future = executor.submit(call_replicate_api, prompt, st.session_state.generate_length)
                 futures.append(future)
                 
             for i, future in enumerate(concurrent.futures.as_completed(futures)):
